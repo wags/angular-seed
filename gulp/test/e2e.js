8d8c04cb4c2bd03fe66db6f42ba74a9f89a3967c
Connor O'Brien <cobrien@four51.com>
2016-10-05 13:04:16 -0500

    E2E test setup
    Getting this task down to one step.

--- gulp/test/e2e.js
+++ gulp/test/e2e.js
@@ -1,17 +1,31 @@
 var gulp = require('gulp'),
-	bowswerSync = require('browser-sync'),
+	browserSync = require('browser-sync').create(),
 	config = require('../../gulp.config'),
 	protractor = require("gulp-protractor").protractor;
 
 gulp.task('start-localhost', ['inject'], function() {
-	
+	browserSync.init({
+        server: {
+            baseDir: [
+                config.root + config.build.replace('.', ''),
+                config.root + config.src.replace('.', '') + 'app/'
+            ],
+            routes: {
+                '/bower_files': config.root + config.bowerFiles.replace('.', '')
+            }
+        },
+        open: false
+    })
 });
 
 gulp.task('test:e2e', ['start-localhost'], function() {
-	gulp.src('./src/**/*.test.js')
+	gulp.src(config.src + '**/*.test.js')
         .pipe(protractor({
             configFile: config.root + '/protractor.conf.js',
         }))
+        .on('end', function() {
+            browserSync.exit();
+        })
         .on('error', function (e) {
             throw e;
         });
